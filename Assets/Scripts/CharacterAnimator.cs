ddbe3087b8f99134e8c0d27bddd67fea51e864a3
kg905812262 <kg905812262@gmail.com>
2018-02-11 21:01:48 +0800

    Search and attack behavior completed
    There is a glitch that the agent sometimes could see through walls.

---
+++ Assets/Scripts/CharacterAnimator.cs
@@ -0,0 +1,72 @@
+﻿using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterAnimator : MonoBehaviour {
+
+	//link to Animator component
+	private Animator animController;
+	//used to set anim controller parameters
+	public enum MoveState { Idle, Walking, Running, Sprinting }
+	public MoveState moveState;
+	//link to NavMeshAgent component
+	private NavMeshAgent navAgent;
+	private float currentSpeed;
+	private BehaviorTree behaviorTree;
+
+	private void Awake()
+	{
+		animController = GetComponent<Animator>();
+		navAgent = GetComponent<NavMeshAgent>();
+		behaviorTree = GetComponent<BehaviorTree>();
+	}
+	private void Start()
+	{
+		currentSpeed = animController.GetFloat("Speed");
+	}
+
+	void Update()
+	{
+		//print(behaviorTree.FindTaskWithName("Can See Object").OnUpdate());
+		var seenObject = (SharedGameObject)behaviorTree.GetVariable("SeenObject");
+		var sqrDist = Vector3.SqrMagnitude(navAgent.destination - transform.position);
+		animController.speed = navAgent.speed;
+		//character walks if there is a navigation path set, idle all other times
+		if (navAgent.hasPath && seenObject.Value != null && sqrDist > Mathf.Pow(navAgent.stoppingDistance * 8f, 2f))
+		{
+			moveState = MoveState.Sprinting;
+		}
+		else if (navAgent.hasPath && seenObject.Value != null && sqrDist > Mathf.Pow(navAgent.stoppingDistance * 5f, 2f))
+		{
+			moveState = MoveState.Running;
+		}
+		else if (navAgent.hasPath && sqrDist > Mathf.Pow(navAgent.stoppingDistance, 2f))
+		{
+			moveState = MoveState.Walking;
+		}
+		else
+		{
+			moveState = MoveState.Idle;
+		}
+		//send move state info to animator controller
+		currentSpeed = Mathf.Lerp(currentSpeed, (float)moveState, navAgent.acceleration * Time.deltaTime);
+		animController.SetFloat("Speed", currentSpeed);
+		
+	}
+
+	void OnAnimatorMove()
+	{
+		//only perform if walking
+		if (moveState != MoveState.Idle)
+		{
+			//set the navAgent's velocity to the velocity of the animation clip currently playing
+			navAgent.velocity = animController.deltaPosition / Time.deltaTime;
+			//set the rotation in the direction of movement
+			Quaternion lookRotation = Quaternion.LookRotation(navAgent.desiredVelocity);
+			transform.rotation = Quaternion.RotateTowards(transform.rotation, lookRotation, navAgent.angularSpeed * Time.deltaTime);
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+	public float speed = 20;
+
+	private Rigidbody rd;
+	private AudioSource footStepsAudio;
+	private float h;
+	private float v;
+
+	private void Awake()
+	{
+		rd = GetComponent<Rigidbody>();
+		footStepsAudio = GetComponent<AudioSource>();
+	}
+
+	private void Start()
+	{
+		h = 0f;
+		v = 0f;
+	}
+
+	private void Update()
+	{
+		h = Input.GetAxis("Horizontal");
+		v = Input.GetAxis("Vertical");
+
+		FootStepsSFX();
+	}
+
+	void FixedUpdate () {
+		Vector3 move = v * transform.forward + h * transform.right;
+		rd.MovePosition(transform.position + move * speed * Time.deltaTime);
+		Quaternion turnRotation = Quaternion.Euler(0f, (Input.GetAxis("Mouse X")) * 5, 0f);
+		rd.MoveRotation(rd.rotation * turnRotation);
+		//transform.Rotate(Vector3.up * (Input.GetAxis("Mouse X")) * 5, Space.World);
+	}
+
+	private void FootStepsSFX()
+	{
+		if (Mathf.Abs(h) < 0.1f && Mathf.Abs(v) < 0.1f)
+		{
+			footStepsAudio.Stop();
+		}
+		else if (!footStepsAudio.isPlaying)
+		{
+			footStepsAudio.Play();
+		}
+
+		if (footStepsAudio.time > 0.35f)
+		{
+			footStepsAudio.time = 0f;
+		}
+	}
+}
